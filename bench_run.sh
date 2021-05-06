648defe4f91ce5dab87268b0283024276aca5d1a
CRZbulabula <crzbulabula@gmail.com>
2021-05-06 11:52:31 +0800

    last commit

--- bench_run.sh
+++ bench_run.sh
@@ -1,16 +1,16 @@
 make clean && make
 cd bench
 ./build.sh
-# ./bench 1 50 0
-# ./bench 1 50 1
-# ./bench 2 50 0
-# ./bench 2 50 1
-# ./bench 3 50 0
-# ./bench 3 50 1
-# ./bench 4 50 0
-# ./bench 4 50 1
+./bench 1 50 0
+./bench 1 50 1
+./bench 2 50 0
+./bench 2 50 1
+./bench 3 50 0
+./bench 3 50 1
+./bench 4 50 0
+./bench 4 50 1
 
-./bench 1 0 0
-./bench 1 100 0
-./bench 1 0 1
-./bench 1 100 1
+# ./bench 1 0 0
+# ./bench 1 100 0
+# ./bench 1 0 1
+# ./bench 1 100 1
